60a58da0706070bf93b770a57bbd58f58d958ce7
Evgeny Shchichilin <eshchichilin@gmail.com>
2017-12-08 12:18:48 +0300

    update 08/12/17

--- src/main/java/DataManager.java
+++ src/main/java/DataManager.java
@@ -1,3 +1,170 @@
-public class DataManager {
-
-}
+//import java.io.BufferedReader;
+//import java.io.ByteArrayInputStream;
+//import java.io.InputStreamReader;
+//import java.math.BigInteger;
+//import java.security.MessageDigest;
+//import java.sql.*;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.zip.GZIPInputStream;
+//
+//public class DataManager {
+//    private static Props prop = Props.getProps();
+//
+//    public Map<String,Object> getOneTestDataByAddressName(String address, String method){
+//        Map<String,Object> map = null;
+//        Connection connection = null;
+//        Statement statement = null;
+//        String query = Context.SQL_GET_ALL_DATA_BY_ADDRESS.replace("{0}", address).replace("{1}",method);
+//        try{
+//            Class.forName("org.postgresql.Driver");
+//            connection = DriverManager.getConnection(prop.CONNECTION_STRING, prop.DB_USER, prop.DB_PWD);
+//            statement = connection.createStatement();
+//            ResultSet result = statement.executeQuery(query);
+//            List<HashMap<String,Object>> list = convertResultSetToMapList(result);
+//            map = list.get(0);
+//        } catch (Exception ex){
+//            ex.printStackTrace();
+//        } finally {
+//            if (statement != null) {
+//                try {
+//                    statement.close();
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//            if (connection != null) {
+//                try {
+//                    connection.close();
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//        }
+//        return map;
+//    }
+//
+//    public String getIdByIdent(String ident){
+//        String id = "";
+//        Map<String,Object> map;
+//        Connection connection = null;
+//        Statement statement = null;
+//        String query = "SELECT * FROM TaskQueue where ident = '" + ident + "'";
+//        try{
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            connection = DriverManager.getConnection(prop.API_TEST_CONNECTION);
+//            statement = connection.createStatement();
+//            ResultSet rs = statement.executeQuery(query);
+//            ResultSetMetaData md = rs.getMetaData();
+//            int columns = md.getColumnCount();
+//            List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
+//            while (rs.next()) {
+//                HashMap<String,Object> row = new HashMap<String, Object>(columns);
+//                for(int i=1; i<=columns; ++i) {
+//                    row.put(md.getColumnName(i),rs.getObject(i));
+//                }
+//                list.add(row);
+//            }
+//            map = list.get(0);
+//            id = map.get("Id").toString();
+//        } catch (Exception ex){
+//            ex.printStackTrace();
+//        } finally {
+//            if (statement != null) {
+//                try {
+//                    statement.close();
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//            if (connection != null) {
+//                try {
+//                    connection.close();
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//        }
+//        return id;
+//    }
+//
+//    private List<HashMap<String,Object>> convertResultSetToMapList(ResultSet rs) throws SQLException {
+//        ResultSetMetaData md = rs.getMetaData();
+//        int columns = md.getColumnCount();
+//        List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
+//        while (rs.next()) {
+//            HashMap<String,Object> row = new HashMap<String, Object>(columns);
+//            for(int i=1; i<=columns; ++i) {
+//                row.put(md.getColumnName(i),rs.getObject(i));
+//            }
+//            list.add(row);
+//        }
+//        return list;
+//    }
+//
+//    public String getZippedString (String id) {
+//        String result = "";
+//        Connection connection = null;
+//        Statement statement = null;
+//        String query = "SELECT * FROM TaskResult where Id = '" + id + "'";
+//        try{
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            connection = DriverManager.getConnection(prop.API_TEST_CONNECTION);
+//            statement = connection.createStatement();
+//            ResultSet rs = statement.executeQuery(query);
+//            ResultSetMetaData md = rs.getMetaData();
+//            int columns = md.getColumnCount();
+//            List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
+//            while (rs.next()) {
+//                HashMap<String,Object> row = new HashMap<String, Object>(columns);
+//                for(int i=1; i<=columns; ++i) {
+//                    row.put(md.getColumnName(i),rs.getObject(i));
+//                }
+//                list.add(row);
+//            }
+//            HashMap<String, Object> first = list.get(0);
+//            byte[] array = (byte[]) first.get("Data");
+//            GZIPInputStream gzis = new GZIPInputStream(new ByteArrayInputStream(array));
+//            InputStreamReader reader = new InputStreamReader(gzis);
+//            BufferedReader in = new BufferedReader(reader);
+//            String readed;
+//            while ((readed = in.readLine()) != null) {
+//                result += readed;
+//            }
+//        } catch (Exception ex){
+//            ex.printStackTrace();
+//        } finally {
+//            if (statement != null) {
+//                try {
+//                    statement.close();
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//            if (connection != null) {
+//                try {
+//                    connection.close();
+//                } catch (SQLException ex) {
+//                    ex.printStackTrace();
+//                }
+//            }
+//        }
+//        return result;
+//    }
+//
+//
+//
+//    private String generateNewHashCode (String id) {
+//        String hash = "";
+//        try {
+//            MessageDigest md5 = MessageDigest.getInstance("MD5");
+//            md5.update(id.getBytes(), 0, id.length());
+//            hash = String.format("%32x", new BigInteger(1, md5.digest()));
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//        return hash;
+//    }
+//}
--- src/test/java/OpenDate_Test.java
+++ src/test/java/OpenDate_Test.java
@@ -1,31 +1,48 @@
+import com.sun.deploy.util.Waiter;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import ru.yandex.qatools.allure.annotations.Description;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.ErrorManager;
 
 import static java.lang.Runtime.getRuntime;
 
 
 public class OpenDate_Test {
+    private static Props prop = Props.getProps();
 
     @Test
-    @Description("1. Запуск сервиса OpenDataService")
-    public void Step_1_start() throws IOException {
-        Runtime a = getRuntime();
-        Process proc = a.exec("cmd /c start C:\\OpenDataService\\OpenDataService.exe / console");
+    @Description("0.1Почищены записи в таблице (OpenDataMasAddress) + ссылки на сохраненный файл")
+    public void Step_0_Preconditions()  {
+        DB_Connect.Delete_All_Rows_From_Table(Context.SQL_Delete_All_Rows_From_OpenDataMasAddress);
+        DB_Connect.Delete_All_Rows_From_Table(Context.SQL_DELETE_ALL_DATA_From_OpenDataPath);
     }
 
     @Test
-    @Description("2. Проверка запуска сервиса OpenDataService")
+    @Description("1. Запуск сервиса OpenDataService")
+    public void Step_1_Start() throws IOException, InterruptedException {
+        Process a = Runtime.getRuntime().exec("cmd /c start C:\\OpenDataService\\OpenDataService.exe / console");
+        TimeUnit.SECONDS.sleep(10);
+    }
+
+    @Test
+    @Description("2. Проверка запуска сервиса OpenDataService") //
+    // - Search for this service in windows processes.
     public void Step_2_CheckAPP() throws IOException {
         Process p = Runtime.getRuntime().exec("tasklist.exe /nh");
         BufferedReader input = new BufferedReader
                 (new InputStreamReader(p.getInputStream()));
         String line;
+        Boolean a;
         while ((line = input.readLine()) != null) {
             if (!line.trim().equals("")) {
-                if (line.substring(0, line.indexOf(" ")).equals("OpenDataService.exe")) {
+                if (a = line.substring(0, line.indexOf(" ")).equals("OpenDataService.exe")){
                     System.out.print("Ура! есть такой процесс!");
                 }
             }
@@ -33,8 +50,38 @@ public class OpenDate_Test {
     }
 
     @Test
-    @Description("Проверка формирования исходящего запроса от сервиса ")
+    @Description("Проверка формирования исходящего(GET) запроса от сервиса")
     public void Step_3_Check_GET_Request() {
+//        HttpRequest<String> httpRequest = HttpRequestBuilder.createGet(uri, String.class)
+//                .responseDeserializer(ResponseDeserializer.ignorableDeserializer()).build();
+    }
 
+    @Test
+    @Description("5. Проверка создания записей в таблице (OpenDataMasAddress) + ссылка на местонахождение файла  *.csv\n")
+    public void Step_4_Check_rows_in_table(){
+        try
+        {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection connection = DriverManager.getConnection(prop.CONNECTION_STRING, prop.DB_USER, prop.DB_PWD);
+            if (connection != null) System.out.println("Connection Successful !\n");
+            if (connection == null) System.exit(0);
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs_OpenDataMassAddress = statement.executeQuery(Context.SQL_GET_ALL_DATA_From_OpenDataMasAddress);
+            ResultSet rs_OpenDataPath = statement.executeQuery(Context.SQL_GET_ALL_DATA_From_OpenDataPath);
+            int Count_Rows = DB_Connect.GetResultSetRowCount(rs_OpenDataMassAddress);
+            int Count_Rows_Path = DB_Connect.GetResultSetRowCount(rs_OpenDataPath);
+
+            if (Count_Rows ==0 && Count_Rows_Path ==0){
+                Assert.assertEquals("В таблице отсутствуют записи", "Rows !=0","Rows ==0");
+            } else System.out.println("Количество записей в таблице OpenDataMassAddress = " + Count_Rows
+                    + "Количество записей в таблице OpenDataPath = "+ Count_Rows_Path);
+            if (rs_OpenDataMassAddress != null) rs_OpenDataMassAddress.close();
+            if (rs_OpenDataPath != null) rs_OpenDataPath.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
