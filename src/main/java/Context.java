83d32e221a51abc3e5a4d098414182d4d57d0b03
Evgeny Shchichilin <eshchichilin@gmail.com>
2017-12-12 16:23:24 +0300

    update 12/12/17

--- src/main/java/Context.java
+++ src/main/java/Context.java
@@ -1,8 +1,6 @@
 public class Context {
     public static String SQL_GET_ALL_DATA_From_OpenDataMasAddress = "SELECT * FROM OpenDataMasAddress";
     public static String SQL_Delete_All_Rows_From_OpenDataMasAddress = "TRUNCATE TABLE  OpenDataMasAddress";
-    public static String SQL_GET_ALL_DATA_From_OpenDataPath = "SELECT * FROM OPENDATAPATH";
-    public static String SQL_DELETE_ALL_DATA_From_OpenDataPath = "TRUNCATE TABLE  OPENDATAPATH";
-    public static String SQL_GET_FIRST_RECORD_From_OpenDataMasAddress = "SELECT Hash FROM OpenDataMasAddress where id = 0;";
-
+    public static String SQL_GET_FIRST_RECORD_From_OpenDataMasAddress = "SELECT  Region,Area,City,Town,Street,House,Building,Flat,EntityNumber " +
+            "FROM OpenDataMasAddress where id = 1";
 }
--- src/main/java/DB_Connect.java
+++ src/main/java/DB_Connect.java
@@ -1,6 +1,3 @@
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.sql.*;
 
 public class DB_Connect {
@@ -20,42 +17,42 @@ public class DB_Connect {
         return size;
     }
 /**--------------------------------------------------------------------------------------------------------------------*/
-@Test
-    public void Check_New_Rows_OpenDataMasAddress()
-    {
-        String Get_All_Data_By_OpenDataMasAddress = Context.SQL_GET_ALL_DATA_From_OpenDataMasAddress;
-        try
-        {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            Connection connection = DriverManager.getConnection(prop.CONNECTION_STRING, prop.DB_USER, prop.DB_PWD);
-            if (connection != null) System.out.println("Connection Successful !\n");
-            if (connection == null) System.exit(0);
-            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            ResultSet resultSet = statement.executeQuery(Context.SQL_GET_FIRST_RECORD_From_OpenDataMasAddress);
-            int Count_Rows = GetResultSetRowCount(resultSet);
-            System.out.println("Количество записей в таблице = "+ Count_Rows);
-            int x = resultSet.getMetaData().getColumnCount(); //Resultset.getMetaData() получаем информацию результирующей таблице
-            while (resultSet.next())
-            {
-                for (int i = 1; i <= x; i++)
-                {
-                    System.out.print(resultSet.getString(i) + "\t");
-                }
-                System.out.println();
-            }
-            if (Count_Rows == 0) {
-                System.out.println("Количество записей в таблице OpenDataMassAddress = " + Count_Rows);
-                Assert.assertEquals("В таблице отсутствуют записи", "Rows !=0", "Rows ==0");
-            } else System.out.println("Количество записей в таблице OpenDataMassAddress = " + Count_Rows);
-            System.out.println();
-            if (resultSet != null) resultSet.close();
-            if (statement != null) statement.close();
-            if (connection != null) connection.close();
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
+
+//    public void Check_New_Rows_OpenDataMasAddress()
+//    {
+//        String Get_All_Data_By_OpenDataMasAddress = Context.SQL_GET_ALL_DATA_From_OpenDataMasAddress;
+//        try
+//        {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            Connection connection = DriverManager.getConnection(prop.CONNECTION_STRING, prop.DB_USER, prop.DB_PWD);
+//            if (connection != null) System.out.println("Connection Successful !\n");
+//            if (connection == null) System.exit(0);
+//            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//            ResultSet resultSet = statement.executeQuery(Context.SQL_GET_FIRST_RECORD_From_OpenDataMasAddress);
+//            int Count_Rows = GetResultSetRowCount(resultSet);
+//            System.out.println("Количество записей в таблице = "+ Count_Rows);
+//            int x = resultSet.getMetaData().getColumnCount(); //Resultset.getMetaData() получаем информацию результирующей таблице
+//            while (resultSet.next())
+//            {
+//                for (int i = 1; i <= x; i++)
+//                {
+//                    System.out.print(resultSet.getString(i) + "\t");
+//                }
+//                System.out.println();
+//            }
+//            if (Count_Rows == 0) {
+//                System.out.println("Количество записей в таблице OpenDataMassAddress = " + Count_Rows);
+//                Assert.assertEquals("В таблице отсутствуют записи", "Rows !=0", "Rows ==0");
+//            } else System.out.println("Количество записей в таблице OpenDataMassAddress = " + Count_Rows);
+//            System.out.println();
+//            if (resultSet != null) resultSet.close();
+//            if (statement != null) statement.close();
+//            if (connection != null) connection.close();
+//        } catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
 
     public static void Delete_All_Rows_From_Table(String Context){
         try
